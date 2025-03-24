b792264701773dd028a539d8ddfe578adda6cf26
Undriling <manashbaruah219@gmail.com>
2025-03-24 22:27:33 +0530

    Title Change

--- src/components/custom/profile.jsx
+++ src/components/custom/profile.jsx
@@ -60,7 +60,7 @@ const Profile = () => {
             className="cursor-pointer px-4 py-2 bg-green-600 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 transition"
             >
                My Resume
-            </button>
+          </button>
         </div>
       </div>
 
